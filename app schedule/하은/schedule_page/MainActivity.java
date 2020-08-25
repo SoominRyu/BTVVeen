6e78b469e88b18157db4b6a31904465e59377360
김하은 <kimhe045@gmail.com>
2020-08-25 17:50:41 +0900

    Add new file

---
+++ app schedule/하은/schedule_page/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.test_scr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView)findViewById(R.id.imageview);
+
+        // drawable에 있는 이미지를 지정합니다.
+        imageView.setImageResource(R.drawable.page1); //page1은 그림 이름이야 drawable 안에 넣어줘야 함
+
+        //버튼 생성(activity_main)에서 확인 가능
+        Button button=findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //인텐트를 통해 main-sub 연결
+                Intent intent=new Intent(MainActivity.this,SubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
