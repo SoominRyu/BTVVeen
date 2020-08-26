8ed2a44349752d4d2bacb38b13d35c9c2b351620
류수민 <82ryusm@naver.com>
2020-08-26 21:33:24 +0900

    나머지 java는 동일!

---
+++ app schedule/수민/날짜,시간선택(+예현 합침)/SubActivity.java
@@ -0,0 +1,184 @@
+package com.example.timetry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.Intent;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import android.widget.Toast;
+
+
+
+
+public class SubActivity extends AppCompatActivity { //이 클래스 이름이 SubActivity
+
+
+    int Hour=0, Minute=0;
+    int year, month, day ;
+    int Hour1,Minute1,year1,month1,day1;
+    TextView mTxtDate;
+    TextView mTxtTime;
+    TextView mTxtDate1;
+    TextView mTxtTime1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        //텍스튜뷰 연결
+        mTxtDate = (TextView)findViewById(R.id.txtdate);
+        mTxtTime = (TextView)findViewById(R.id.txttime);
+        mTxtDate1 = (TextView)findViewById(R.id.txtdate1);
+        mTxtTime1 = (TextView)findViewById(R.id.txttime1);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageview);
+
+        // drawable에 있는 이미지를 지정합니다.
+        imageView.setImageResource(R.drawable.page2);//page2는 그림 이름
+
+
+        //버튼 설정
+        // sub2로 이동
+        Button button2 = findViewById(R.id.btn2); //btn2라고 내가 생성한 거(activity_sub2에서 확인 가능)
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity.this, Sub2Activity.class);
+                startActivity(intent);
+
+                //    finish();//초기 화면으로 이동
+            }
+        });
+        //main으로 이동
+        Button button3 = findViewById(R.id.btn4); //btn2라고 내가 생성한 거(activity_sub2에서 확인 가능)
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SubActivity.this, MainActivity.class);
+                startActivity(intent);
+
+                //    finish();//초기 화면으로 이동
+            }
+        });
+
+        //Time
+        Button button6 = findViewById(R.id.btn6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog
+                        = new TimePickerDialog(SubActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String apm = "";
+                        if (hourOfDay < 12){
+                            if(hourOfDay == 0) hourOfDay = 12;
+                            apm = "AM";
+                        }
+                        else {
+                            if (hourOfDay != 12) hourOfDay -=12;
+                            apm = "PM";
+                        }
+                        mTxtTime.setText(String.format("%d:%02d %s", hourOfDay, minute,apm)); //시,분 받아서 보내줌
+                    }
+
+                },Hour, Minute, false);
+
+                timePickerDialog.show();
+            }
+        });
+
+        //date
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day= calendar.get(Calendar.DAY_OF_MONTH);
+
+        Button button5 = findViewById(R.id.btn5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SubActivity.this, dateSetListener, year, month, day).show();
+            }
+        });
+        //Time1
+        Button button8 = findViewById(R.id.btn8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog timePickerDialog1
+                        = new TimePickerDialog(SubActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay1, int minute1) {
+                        String apm1 = "";
+                        if (hourOfDay1 < 12){
+                            if(hourOfDay1 == 0) hourOfDay1 = 12;
+                            apm1 = "AM";
+                        }
+                        else {
+                            if (hourOfDay1 != 12) hourOfDay1 -=12;
+                            apm1 = "PM";
+                        }
+                        mTxtTime1.setText(String.format("%d:%02d %s", hourOfDay1, minute1,apm1)); //시,분 받아서 보내줌
+                    }
+{
+                    }
+                },Hour1, Minute1, false);
+
+                timePickerDialog1.show();
+            }
+        });
+
+        //date1
+        GregorianCalendar calendar1 = new GregorianCalendar();
+        year1 = calendar1.get(Calendar.YEAR);
+        month1 = calendar1.get(Calendar.MONTH);
+        day1 = calendar1.get(Calendar.DAY_OF_MONTH);
+
+        Button button7 = findViewById(R.id.btn7);
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(SubActivity.this, dateSetListener1, year1, month1, day1).show();
+
+            }
+        });
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            String msg = String.format("%d / %d / %d", year,monthOfYear+1, dayOfMonth);
+            Toast.makeText(SubActivity.this, msg, Toast.LENGTH_SHORT).show();
+            mTxtDate.setText(String.format("%d/%d/%d", year, monthOfYear + 1, dayOfMonth));
+        }
+    };
+
+    private DatePickerDialog.OnDateSetListener dateSetListener1 = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year1, int monthOfYear1,
+                              int dayOfMonth1) {
+            // TODO Auto-generated method stub
+            String msg1 = String.format("%d / %d / %d", year1,monthOfYear1+1, dayOfMonth1);
+            Toast.makeText(SubActivity.this, msg1, Toast.LENGTH_SHORT).show();
+            mTxtDate1.setText(String.format("%d/%d/%d", year1, monthOfYear1 + 1, dayOfMonth1));
+        }
+    };
+
+}
